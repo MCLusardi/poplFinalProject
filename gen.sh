c9769489b83971abeb48750797fc9a8342716e70
Stuart <sa5f5@umsystem.edu>
2022-11-09 08:17:59 -0600

    updated gen script to error if no grammar file provided

--- gen.sh
+++ gen.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+if [ -z "$1" ]
+    then
+        echo "No grammar file supplied"
+        echo "Suggested usage:"
+        echo "./gen.sh popl.g4"
+        exit 1
+fi
 ANTLR="antlr-4.11.1-complete.jar"
 rm -rf parserlibs
 mkdir parserlibs
