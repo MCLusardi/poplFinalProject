4c58a2595732b40a077eb3e7be2a8f2f0f9300d8
Stuart <sa5f5@umsystem.edu>
2022-11-08 19:52:23 -0600

    fixed parser gen and added main program

--- gen.sh
+++ gen.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+ANTLR="antlr-4.11.1-complete.jar"
 rm -rf parserlibs
 mkdir parserlibs
-cp antlr-4.9.2-complete.jar parserlibs/antlr-4.9.2-complete.jar
+cp $ANTLR parserlibs/$ANTLR
 cp $1 parserlibs/$1
 cd parserlibs
-java -jar antlr-4.9.2-complete.jar -Dlanguage=Python3 $1
-
+java -jar $ANTLR -Dlanguage=Python3 $1
--- setup.sh
+++ setup.sh
@@ -1,3 +1,2 @@
 #!/bin/bash 
 pip install antlr4-python3-runtime
-
