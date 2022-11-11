a43c2d8f851bef463bd39cc92d402d6623f55f72
MCLusardi <mclntg@mail.missouri.edu>
2022-11-11 14:49:08 -0600

    refactored rules and added support for standalones

--- testFile.py
+++ testFile.py
@@ -1,5 +1,5 @@
 test=1
-
+4
 test = 2
 _test=3
 test1=4
@@ -7,17 +7,18 @@ test+=1
 test-=2
 test = 33.574
 test+=2341.99999999999999999
-
+"hello"
 test/=3
 test*=4
 test_=1
 test_1=2
-
+rando_var
 test = 3+1*4/12
 test = var
 
-test = "hi"
-test = 'hi'
+test1 = "hi"
+test2 = 'hi'
+another_test = test1 + test2
 
 test = 0x10
 test = 0xa1f
